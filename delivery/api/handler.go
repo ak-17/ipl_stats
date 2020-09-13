dc604f95af293f29bfafc8aec2f493c5f045b833
ak-17 <t.akshaya.kumar17@gmail.com>
2020-09-13 21:43:58 +0530

    cors added

--- delivery/api/handler.go
+++ delivery/api/handler.go
@@ -10,8 +10,16 @@ import (
 
 type HandlerFunc func(rw http.ResponseWriter, r *http.Request) (interface{}, error)
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func (fn HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	response := Response{}
+
+	setupResponse(&w, r)
 	start := time.Now()
 
 	data, err := fn(w, r)
